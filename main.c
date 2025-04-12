517c86f2d485da912a6c3600bbbc549709b183de
AgichevDS <agichev.dmitri@gmail.com>
2025-04-12 16:00:54 +0300

    123

--- main.c
+++ main.c
@@ -9,10 +9,10 @@ int main(){
     stack *head = alloc();
     list *ls = lalloc();
     printf("Developer AgichevDS\n");
-    while(state != 5){
+    while(state != 6){
         printf("List items:\n");
         loutput(ls);
-        printf("Select action\n1)ADD\n2)DEL\n3)COPY\n4)OUTPUT\n5)EXIT\n");
+        printf("Select action\n1)ADD\n2)DEL\n3)COPY\n4)OUTPUT\n5)CHANGE\n6)EXIT\n");
         fgets(buffer, max_len_string, stdin);
         del_space(buffer);
         system("cls");
@@ -20,7 +20,7 @@ int main(){
             state = atoi(buffer);
             if(state == 1){
                 clean(head);
-                head = add_stack();
+                head = add_stack(alloc());
                 printf("Enter ID\n");
                 fgets(buffer, max_len_string, stdin);
                 del_space(buffer);
@@ -61,6 +61,19 @@ int main(){
                 system("cls");
             }
             else if(state == 5){
+                clean(head);
+                printf("Enter ID\n");
+                fgets(buffer, max_len_string, stdin);
+                del_space(buffer);
+                id = (size_t)atoi(buffer);
+                if(access(ls, id, ls) != NULL){
+                    head = add_stack(copy(elem(access(ls, id, ls))));
+                    ls = del_list(access(ls, id, ls));
+                    add_list(ls, copy(head), id);
+                }
+                system("cls");
+            }
+            else if(state == 6){
                 printf("End of work");
             }
             else{
